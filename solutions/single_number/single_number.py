26f4fa95e7c94cd1bc3e412f9136f46e70e36b98
Waldi <golubnichij.ve@phystech.edu>
2022-09-24 14:53:44 +0800

    Added Single Number solution

---
+++ solutions/single_number/single_number.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for val in nums:
+            xor ^= val
+        return xor
+
+    def singleNumber1(self, nums: List[int]) -> int:
+        hashset = set()
+        for val in nums:
+            if val not in hashset:
+                hashset.add(val)
+            else:
+                hashset.remove(val)
+        return hashset.pop()
+
+
+def main():
+    solution = Solution()
+    nums = [4, 4, 5, 5, 1, 2, 2, 1, 7, 8, 8]
+    print(nums, solution.singleNumber(nums))
+
+
+if __name__ == '__main__':
+    main()
